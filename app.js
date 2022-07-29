eb50abdaf4903daebdc6d115f4f4880952bd0f4a
yeqin <892280082@qq.com>
2022-07-29 12:28:18 +0800

    auto commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const {SpringBoot} = require("spring-ioc")
 const {SpringIocMvcScaner} = require("./spring_extends")
 
+//根目录以启动js文件为准
 const app = new SpringBoot({srcList:["./demo"],moduleList:[SpringIocMvcScaner("./spring_extends")]});
 
 module.exports = {app}
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const {app} = require('./app')
 
-//只编译，然后手动运行.runtemp.js
-//app.build();
 
 //编译+自动运行.runtemp.js
 app.start();
