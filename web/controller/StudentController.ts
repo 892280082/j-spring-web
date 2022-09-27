a2858b174a6b6b4b6bc54225782e4b4fe65b75fd
yeqin <892280082@qq.com>
2022-09-27 19:15:39 +0800

    add md

--- web/controller/StudentController.ts
+++ web/controller/StudentController.ts
@@ -4,6 +4,37 @@ import { Component } from "j-spring";
 import { ApiMiddleWare, Controller, ExpressMiddleWare, Get, Json, MiddleWare, Param, PathVariable, RequestParam, ResponseBody, SessionAttribute } from "../../src";
 
 
+
+//定义控制器
+@Controller('/student')
+export class StudentController {
+
+    //页面渲染
+    @Get()
+    async index(){
+        return ['index.ejs',{msg:'hello world'}]
+    }
+
+    //接口返回
+    @Get('/getStudentInfo/:id')
+    @ResponseBody()
+    async getStudentInfo(
+        @PathVariable('id') id:string,
+        @RequestParam('name') name:string){
+        return {id,name}
+    }
+
+
+    @Get()
+    @ResponseBody()
+    async addSessionName(@Param('session') session:any){
+        session['name'] = 'xiaoAi'
+        return {msg:'add success!'}
+    }
+
+}
+
+
 @Component
 class XiaoAiMustBeExist implements ExpressMiddleWare {
     isExpressMidldleWare(): boolean {
@@ -16,33 +47,6 @@ class XiaoAiMustBeExist implements ExpressMiddleWare {
         next();
     }
     
-}
-
-
-@Controller('/student')
-export class StudentController {
-
-
-    @Get()
-    async index(){
-        return ['index.ejs',{msg:'hello world'}]
-    }
-
-
-    @Get('/getStudentInfo/:id')
-    @ResponseBody()
-    async getStudentInfo(@PathVariable('id') id:string,@RequestParam('name') name:string,@Param('req') req:any){
-        return {id,name}
-    }
-
-
-    @Get()
-    @ResponseBody()
-    async addSessionName(@Param('session') session:any){
-        session['name'] = 'xiaoAi'
-        return {msg:'add success!'}
-    }
-
 }
 
 
